4fd40bc7c7215883425c5ece26bec4f8ff10a0a8
marun <run@runrun520.cn>
2019-06-07 15:06:59 +0000

    回复request

--- routes/web.php
+++ routes/web.php
@@ -17,47 +17,47 @@ Auth::routes(['verify' => true]);
 
 
 // 中间件，auth 需要用户登陆，verified 邮箱检测过的
-Route::group(['middleware' => ['auth','verified']],function () {
+Route::group(['middleware' => ['auth', 'verified']], function () {
 
     // 关于用户地址的路由
-    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
-    Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
-    Route::post('user_addresses/create','UserAddressesController@store')->name('user_addresses.store');
-    Route::get('user_addresses/{address}','UserAddressesController@edit')->name('user_addresses.edit');
-    Route::put('user_addresses/{address}','UserAddressesController@update')->name('user_addresses.update');
-    Route::delete('user_addresses/{address}','UserAddressesController@destroy')->name('user_addresses.destroy');
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses/create', 'UserAddressesController@store')->name('user_addresses.store');
+    Route::get('user_addresses/{address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+    Route::put('user_addresses/{address}', 'UserAddressesController@update')->name('user_addresses.update');
+    Route::delete('user_addresses/{address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
     // 收藏商品和取消收藏 我的收藏列表
-    Route::post('products/{product}/favorite','ProductsController@favor')->name('products.favor');
-    Route::delete('products/{product}/favorite','ProductsController@disfavor')->name('products.disfavor');
-    Route::get('products/favorites','ProductsController@favorites')->name('products.favorites');
+    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
     // 购物车相关
-    Route::post('cart','CartItemsController@add')->name('cart.add');
-    Route::get('cart','CartItemsController@index')->name('cart.index');
-    Route::delete('cart/{sku}','CartItemsController@remove')->name('cart.remove');
+    Route::post('cart', 'CartItemsController@add')->name('cart.add');
+    Route::get('cart', 'CartItemsController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartItemsController@remove')->name('cart.remove');
 
     // 订单相关
-    Route::post('order','OrdersController@store')->name('orders.store');
-    Route::get('order','OrdersController@index')->name('orders.index');
-    Route::get('order/{order}','OrdersController@show')->name('orders.show');
+    Route::post('order', 'OrdersController@store')->name('orders.store');
+    Route::get('order', 'OrdersController@index')->name('orders.index');
+    Route::get('order/{order}', 'OrdersController@show')->name('orders.show');
     Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
 
     // 支付相关
-    Route::get('payment/{order}/alipay','PaymentController@payByAlipay')->name('payment.alipay');
-    Route::get('payment/alipay/return','PaymentController@alipayReturn')->name('payment.alipay.return');
-    Route::get('payment/{order}/wechat','PaymentController@payByWechat')->name('payment.wechat');
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+    Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
 
 });
 
 // 无需登陆 认证
 // 首页 商品列表
-Route::redirect('/','/products')->name('root');
-Route::get('/products','ProductsController@index')->name('products.index');
+Route::redirect('/', '/products')->name('root');
+Route::get('/products', 'ProductsController@index')->name('products.index');
 
 // 商品详情页
-Route::get('/products/{product}','ProductsController@show')->name('products.show');
+Route::get('/products/{product}', 'ProductsController@show')->name('products.show');
 // 支付宝异步回调
-Route::post('payment/alipay/notify','PaymentController@alipayNotify')->name('payment.alipay.notify');
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 // 微信支付异步回调
-Route::get('payment/wechat/notify','PaymentController@wechatNotify')->name('payment.wechat.notify');
+Route::get('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
