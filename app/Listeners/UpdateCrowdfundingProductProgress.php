ff6646482b721d5939c4acef31c0d1430f9f10a2
marun <marun@idanchuang.com>
2019-07-15 16:20:19 +0800

    Git Push Test3

--- app/Listeners/UpdateCrowdfundingProductProgress.php
+++ app/Listeners/UpdateCrowdfundingProductProgress.php
@@ -9,23 +9,20 @@ use Illuminate\Database\Eloquent\Builder;
 
 class UpdateCrowdfundingProductProgress implements ShouldQueue
 {
-
     /**
      * Handle the event.
      *
-     * @param  OrderPaid $event
-     * @return void
+     * @param OrderPaid $event
      */
     public function handle(OrderPaid $event)
     {
         $order = $event->getOrder();
         // 如果订单类型不是众筹商品订单，无需处理
-        if ($order->type !== Order::TYPE_CROWDFUNDING) {
+        if (Order::TYPE_CROWDFUNDING !== $order->type) {
             return;
         }
 
         $crowdfunding = $order->items[0]->product->crowdfunding;
-
 
         $data = Order::query()
             ->where('type', Order::TYPE_CROWDFUNDING)
@@ -40,7 +37,7 @@ class UpdateCrowdfundingProductProgress implements ShouldQueue
 
         $crowdfunding->update([
             'total_amount' => $data->total_amount,
-            'user_count'   => $data->user_count,
+            'user_count' => $data->user_count,
         ]);
     }
 }
